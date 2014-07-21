ad186e4470172f364bcceec6fa37eb3f093491ff
Thierry Passeron <thierry.passeron@gmail.com>
2014-07-21 11:27:33 +0200

    init0

---
+++ README.md
@@ -0,0 +1,125 @@
+# Dumblog
+
+Why? Because.
+
+## Requirement
+
+none
+
+## Install
+
+```js
+$ npm i dumblog --save
+``` 
+
+## Usage
+
+```js
+var Blog = require('dumblog').Blog;
+
+var blog = new Blog(__dirname + '/blog/articles', {
+  fileRegExp: /\.md$/
+});
+
+var articles = blog.articles();
+````
+
+This will find all articles matching '*.md' in blog/articles directory.
+
+## Articles
+
+You may write articles in any format you like but, dumblog expects to find email-like meta information at the beginning of each article files __AND__ an __EMPTY__ line before the content:
+
+```txt
+from: orion
+subject: New article!
+foo: bar
+
+The article's content starts here after a __REQUIRED__ empty line...
+```
+
+
+
+## Meta 
+
+You can put any meta attributes in your articles. 
+If an attribute is expected to be an array, then separate each elements with commas like so:
+
+```txt
+tags: foo, bar, baz
+```
+
+And of course you need to tell dumblog that the 'tags' meta is an array. Why? Because Dumblog is so dumb!
+
+```js
+var blog = new Blog(__dirname + '/blog/articles', {
+  multiple: ['tags']
+});
+````
+
+By default, meta.tags is known (Huh.. at least one thing it knows :) to be an array.
+
+You can also define default metas for articles:
+```js
+var blog = new Blog(__dirname + '/blog/articles', {
+  meta: {
+    from: 'me',
+    subject: 'no idea'
+  }
+});
+````
+Each article's metas will override these default values.
+
+### Accessors
+
+An article has 2 main accessors:
+
+* meta: the meta information read while parsing the file. It is an Object of key/values
+* content: the raw content as read while parsing the file. It is a String.
+
+```js
+var article = articles[0];
+console.log(article.meta.from, article.content);
+````
+
+On top of which article file modification and access time are automatically recorded in the meta object:
+```js
+var article = articles[0];
+console.log(article.meta.modified_on, article.meta.accessed_on);
+````
+They are both Date objects
+
+
+
+## Callbacks
+
+To very easily accomodate any format like __markdown__, __textile__ etc ... Dumblog is totally agnostic (Heh!).
+
+What you should do is provide your own transformation to the articles' raw content in a "onArticle" callback like so:
+
+```js
+var 
+  marked = require('marked')
+  
+  blog = new Blog(__dirname + '/blog/articles', {
+    onArticle: function (article) {
+      article.HTMLContent = marked(article.content);
+    }
+  });
+````
+
+You can also provide a sort function so that articles will be sorted the right way when they are fetched:
+```js
+var blog = new Blog(__dirname + '/blog/articles', {
+  sort: function (a, b) {
+    return a.meta.modified_on > b.meta.modified_on
+  }
+});
+````
+
+
+## License
+
+Copyright (c) 2014, Thierry Passeron
+MIT License
+
---
+++ index.js
@@ -0,0 +1,165 @@
+/**
+
+  Blog model
+
+  @param articlesPath
+    full path of the articles
+
+  @param options
+    * meta, default: {}                           // The default meta
+    * multiple: ['tags']                          // Which meta keys hold multiple values
+    * fileRegExp: default: /\.txt$/               // Articles matching file names regexp
+    * onArticle(new article): default noop        // Callback when an Article object is created
+    * sort(Article a, Article b): default noop    // sort articles function
+
+  @method articles()
+    returns all the articles found and cache it for next calls
+    Rq: articles are those that can be found at the articlesPath and match the fileRegExp. 
+      If you don't want an article to be added to the articles cache list you may set it's meta.status to something else than "published"
+      which is set automatically during import. This can easily be done in a onArticle callback.
+
+  @method reload()
+    reload articles
+
+  Usage:
+  ======
+
+    var blog = new Blog('/path/to/articles', { 
+      fileRegExp: /\.md$/, 
+      onArticle: function (article) { article.HTMLContent = marked(article.content); },
+      sort: function (a, b) { return a.modified_on < b.modified_on; }
+    });
+
+    var articles = blog.articles();
+*/
+
+function Blog(articlesPath, options) {
+  if (!fs.existsSync(articlesPath) && !fs.mkdirSync(articlesPath)) {
+    throw new Error('Could not create articles base dir for blog at ' + articlesPath);
+  }
+  
+  this.options = options;
+  this.options.meta = this.options.meta || this.Defaults.meta;
+  this.options.multiple = this.options.multiple || this.Defaults.multiple;
+  this.options.fileRegExp = this.options.fileRegExp || this.Defaults.fileRegExp;
+  
+  this.articlesPath = articlesPath;
+  this._articles = this.articles();
+}
+
+Blog.prototype.Defaults = {
+  meta : {
+    from: "Unknown author",
+    subject: "Unknown subject",
+    status: "published",
+    tags: []
+  },
+  multiple: ['tags'],
+  sort: null,
+  fileRegExp: /\.txt/,
+  onArticle: null 
+}
+
+Blog.prototype.reload = function () {
+  this._articles = null;
+  this._articles = this.articles();
+}
+
+Blog.prototype.articles = function () {
+  if (this._articles) return this._articles;
+  
+  var articles = [];
+  var self = this;
+  fs.readdirSync(this.articlesPath).forEach(function (file) {
+    if (file.match(self.options.fileRegExp)) {
+      var article = new Article(self.articlesPath + '/' + file, self.options);
+      article.meta.status = "published";
+      if (self.options.onArticle) self.options.onArticle(article);
+      if (article.meta.status == 'published') articles.push(article);
+    }
+  });
+  if (this.options.sort) articles = articles.sort(this.options.sort);
+  return articles;
+};
+
+
+/**
+
+  Article model
+
+  @param filePath
+    full path to the article file
+  @param options
+    * multiple: ['tags']    // Specify the meta keys that hold multiple values
+    $ meta: {}
+
+  @attr content
+    returns the content part (String)
+  @attr meta
+    returns the meta part (Object)
+
+  Usage:
+  ======
+
+  var article = new Article('/path/to/article');
+  console.log(article.content, article.meta);
+
+
+  Article format:
+  ---------------
+  
+  We parse meta data in email-like format from the top of the document until an empty line is found. 
+  After this new line it is assumed that we have the text content. Thus, if you don't need any meta you should
+  take care of having a first empty line.
+
+  <example.md>:
+  from: orion
+  subject: hello world!
+  tags: foo, bar, baz
+
+  # Hello
+  world!
+  ...  
+
+*/
+
+function Article(filePath, options) {
+  this.options = options;
+  
+  this.meta = {};
+  for (var key in this.options.meta) {
+    if (this.options.multiple.indexOf(key) >= 0) this.meta[key] = this.options.meta[key].slice(0); // Copy array
+    else this.meta[key] = this.options.meta[key];
+  }
+  
+  var stat = fs.statSync(filePath)
+  this.meta.modified_on = stat.mtime;
+  this.meta.accessed_on = stat.atime;
+  
+  this.tidy(fs.readFileSync(filePath).toString());  
+}
+
+Article.prototype.tidy = function (raw) {
+  var content = [];
+  var meta = this.meta;
+  var multiple = this.options.multiple || [];
+  var hadEmptyLine = false;
+  raw.split("\n").forEach(function (line) {
+    if (line.match(/^$/)) hadEmptyLine = true;
+    if (hadEmptyLine) content.push(line);
+    else {
+      var metaMatch = line.match(/^\s*([a-z]+)\s*:\s*(.*)$/);
+      if (metaMatch) { // Ex: foo: anything, bar, baz
+        var key = metaMatch[1], string = metaMatch[2];
+        var values = string.split(/\s*,\s*/).map(function (s){ return s.trim(); });
+        meta[key] = multiple.indexOf(key) >= 0 ? values : values.join(', ');
+      }
+    } 
+  });
+  
+  this.content = content.join("\n");
+  this.meta = meta;
+}
+
+module.exports.Blog = Blog;
+module.exports.Article = Article;
